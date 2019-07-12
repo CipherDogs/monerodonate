29d5d9a0142322c1ace60418cd678e2c1aa4fc02
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-07-12 23:40:35 +0700

    Added files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get('address') !== null) {
+    document.getElementById("view").style.display = 'flex';
+    document.getElementById("send").innerHTML = urlParams.get('address');
+    new QRCode(document.getElementById("qrcode"), urlParams.get('address'));
+} else {
+    document.getElementById("generate").style.display = 'flex';
+}
+
+function generate() {
+    let address = document.getElementById("address").value;
+    let result = '<a href="' + location.href + "?address=" + address + '"><img height="100" width="100" src="' + location.href + '/img/monero.png"></a>';
+    document.getElementById("result").value = result;
+    document.getElementById("html").innerHTML = result;
+}
